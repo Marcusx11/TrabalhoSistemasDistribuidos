8984bb477d282b9ca07e72c4442d3ef96c0da408
Gabriel Augusto <gabrielfrzy@gmail.com>
2021-07-15 10:44:18 -0300

    rename client class

--- src/client/Main.java
+++ src/client/Main.java
@@ -7,7 +7,7 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-public class Client {
+public class Main {
 
     public static void main(String[] args) {
         try {
