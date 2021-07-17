0ee80a12bf201f9982187b4c22bb482cdeb565e3
thiago <thenrique2012@gmail.com>
2021-07-17 17:05:17 -0300

    add database_url in Constants.java

--- src/core/Constants.java
+++ src/core/Constants.java
@@ -1,7 +1,14 @@
 package core;
 
+import java.io.File;
+
 public class Constants {
     public static final String CHANNEL_CLUSTER_NAME = "bankGroupCluster";
     public static final String RMI_NAME = "bank";
     public static final String RMI_URL = "//localhost/bank";
+
+    private static final File file = new File("bank.sqlite");
+    public static final String DATABASE_URL = "jdbc:sqlite:" + file.getAbsolutePath();
+
+
 }
