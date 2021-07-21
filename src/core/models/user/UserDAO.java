c66357c25fcbd9aedfeba6c4a59569208d55b944
Gabriel Augusto <gabrielfrzy@gmail.com>
2021-07-21 11:50:13 -0300

    resolvendo conflitos

--- src/core/models/user/UserDAO.java
+++ src/core/models/user/UserDAO.java
@@ -9,7 +9,6 @@ public class UserDAO {
     public void create(User user) {
         try (Connection connection = ConnectionFactory.getConnection()) {
             String sql = "INSERT INTO users (name, cpf, password, online, id) VALUES (?, ?, ?, ?, ?)";
-
             PreparedStatement stmt = connection.prepareStatement(sql);
 
             stmt.setString(1, user.getName());
@@ -25,26 +24,7 @@ public class UserDAO {
         }
     }
 
-    private List<User> getUsers(PreparedStatement stmt) throws SQLException {
-        List<User> users = new ArrayList<User>();
-        ResultSet results = stmt.executeQuery();
-
-        while (results.next()) {
-            User user = new User();
-            user.setCpf(results.getString("cpf"));
-            user.setName(results.getString("name"));
-            user.setPassword(results.getString("password"));
-
-            users.add(user);
-        }
-
-        results.close();
-        stmt.close();
-
-        return users;
-    }
-
-    public User findBy(String field, String where) {
+    public void update(User user) {
         try (Connection connection = ConnectionFactory.getConnection()) {
             String sql = "UPDATE users SET " +
                 "name = ?, " +
@@ -52,9 +32,7 @@ public class UserDAO {
                 "online = ? " +
                 "WHERE id = ?";
 
-
             PreparedStatement stmt = connection.prepareStatement(sql);
-            stmt.setString(1, field + where);
 
             stmt.setString(1, user.getName());
             stmt.setString(2, user.getCpf());
@@ -63,13 +41,12 @@ public class UserDAO {
 
             stmt.execute();
             stmt.close();
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static User findByCpfAndPassword(String cpf, String password) {
+    public User findBy(String field, String value) {
         try (Connection connection = ConnectionFactory.getConnection()) {
             Statement statement = connection.createStatement();
             ResultSet results = statement.executeQuery("SELECT * FROM users WHERE " + field + " = " + value + " LIMIT 1");
@@ -85,13 +62,12 @@ public class UserDAO {
             }
 
             return user;
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public List<User> selectBy(String field, String where) {
+    public List<User> selectAll() {
         try (Connection connection = ConnectionFactory.getConnection()) {
             Statement statement = connection.createStatement();
             ResultSet results = statement.executeQuery("SELECT * FROM users");
@@ -105,17 +81,10 @@ public class UserDAO {
                 user.setPassword(results.getString("password"));
                 user.setOnline(results.getInt("online"));
 
-            return getUsers(stmt);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+                users.add(user);
+            }
 
-    public List<User> selectAll() {
-        try (Connection connection = ConnectionFactory.getConnection()) {
-            String sql = "SELECT * FROM users";
-            PreparedStatement stmt = connection.prepareStatement(sql);
-            return getUsers(stmt);
+            return users;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
