be15371307a3d65ed537b2b98ec0408f933ded51
Mustardking <44874254+Marcusx11@users.noreply.github.com>
2021-07-21 22:04:59 -0300

    algumas mudança na view

--- src/client/Main.java
+++ src/client/Main.java
@@ -23,30 +23,38 @@ public class Main {
             bank = (BankInterface) Naming.lookup(Constants.RMI_URL);
             input = new Scanner(System.in);
 
-            viewInitialMenu();
+            switchBetweenMenus();
         } catch (NotBoundException | MalformedURLException | RemoteException e) {
             e.printStackTrace();
         }
     }
 
-    public static void viewInitialMenu() throws RemoteException{
+    public static void switchBetweenMenus() throws RemoteException {
         while (true) {
-            System.out.println("**** Menu ****");
-            System.out.println("[1] - Login");
-            System.out.println("[2] - Register");
-            System.out.print("> ");
-
-            String option = input.nextLine();
-            switch (option) {
-                case "1":
-                    viewLogin();
-                    break;
-                case "2":
-                    viewRegister();
-                    break;
-                default:
-                    System.out.println("invalid option");
+            if (authUser == null) {
+                viewInitialMenu();
+            } else {
+                viewDashboardMenu();
             }
+        }
+    }
+
+    public static void viewInitialMenu() throws RemoteException{
+        System.out.println("**** Menu ****");
+        System.out.println("[1] - Login");
+        System.out.println("[2] - Register");
+        System.out.print("> ");
+
+        String option = input.nextLine();
+        switch (option) {
+            case "1":
+                viewLogin();
+                break;
+            case "2":
+                viewRegister();
+                break;
+            default:
+                System.out.println("invalid option");
         }
     }
 
@@ -56,6 +64,8 @@ public class Main {
         System.out.println("Ações");
         System.out.println("[1] - Verificar saldo");
         System.out.println("[2] - Transferencia");
+        System.out.println("[3] - Verificar extrato");
+        System.out.println("[4] - Verificar montante do banco");
         System.out.print("> ");
 
         String option = input.nextLine();
@@ -63,7 +73,14 @@ public class Main {
             default:
                 System.out.println("invalid option");
         }
+
     }
+
+    // TODO implementar depois da classe tranf. ser feita
+    public static void viewSaldo() throws RemoteException {}
+    public static void viewTransferencia() throws RemoteException {}
+    public static void viewVerificarExtrato() throws RemoteException {}
+    public static void viewVerificarMontanteBanco() throws RemoteException {}
 
     public static void viewRegister() throws RemoteException {
         System.out.println("**** Register ****");
